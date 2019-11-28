a61d71929623b2c9da5d4d36a83144754219feb1
Anthony Sherwin <anthony.sherwin@essential.net.au>
2019-11-28 14:57:55 +1100

    Fixing up the build and aws support.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
 import Navigation from '../components/nav'
-import About from '../pages/about'
-import Experience from '../pages/experience'
-import Education from '../pages/education'
-import Skills from '../pages/skills'
-import Interests from '../pages/interests'
-import Awards from '../pages/awards'
+import About from '../components/about'
+import Experience from '../components/experience'
+import Education from '../components/education'
+import Skills from '../components/skills'
+import Interests from '../components/interests'
+import Awards from '../components/awards'
 import { Container } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Scrollspy } from 'reactstrap-scrollspy'
@@ -14,32 +14,40 @@ import { Scrollspy } from 'reactstrap-scrollspy'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/style.css"
 
-const Home = () => (
-  <div id="page-top">
-    <Head>
-      <title>Anthony Sherwin Resume</title>
-      <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"/>
-      <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet"/>
-    </Head>
+class Home extends React.Component {
+  static getInitialProps(ctx) {
+    return {};
+  }
 
-    <Scrollspy
-      names={['navbar', 'about', null, 'experience', null, 'education', null, 'skills', null, 'interests', null, 'awards']}
-      homeIndex={1}
-    >
-      <Navigation />
-      <About />
-      <hr className="m-0" />
-      <Experience />
-      <hr className="m-0" />
-      <Education />
-      <hr className="m-0" />
-      <Skills />
-      <hr className="m-0" />
-      <Interests />
-      <hr className="m-0" />
-      <Awards />
-    </Scrollspy>
-  </div>
-)
+  render() {
+    return (
+      <div id="page-top">
+        <Head>
+          <title>Anthony Sherwin Resume</title>
+          <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet"/>
+        </Head>
+
+        <Scrollspy
+          names={['navbar', 'about', null, 'experience', null, 'education', null, 'skills', null, 'interests', null, 'awards']}
+          homeIndex={1}
+        >
+          <Navigation />
+          <About />
+          <hr className="m-0" />
+          <Experience />
+          <hr className="m-0" />
+          <Education />
+          <hr className="m-0" />
+          <Skills />
+          <hr className="m-0" />
+          <Interests />
+          <hr className="m-0" />
+          <Awards />
+        </Scrollspy>
+      </div>
+    )
+  }
+}
 
 export default Home
