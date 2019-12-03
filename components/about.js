73c6809811a46e4ce3faf3d9421bb528f2d6ae46
Anthony Sherwin <anthony.sherwin@essential.net.au>
2019-12-03 11:51:19 +1100

    Cleaned up about portion and finalised the content.

--- components/about.js
+++ components/about.js
@@ -4,45 +4,64 @@ import { Row, Col } from 'reactstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
-library.add(fab)
 
-const About = () => (
+class About extends React.Component {
 
-  <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
-    <div className="w-100">
-      <Row>
-        <Col>
-          <h1 className="mb-0">Anthony
-            <span className="text-primary"> Sherwin</span>
-          </h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2 className="subheading mb-5">70A Shady Grove, Forest Hill, 3131
-            <a href="mailto:anthony.j.sherwin@gmail.com"> anthony.j.sherwin@gmail.com</a>
-          </h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
-        </Col>
-      </Row>
-      <Row className="social-icons">
-        <Col xs="1" className="mr-3">
-        <a href="#">
-          <FontAwesomeIcon icon={['fab', 'linkedin']} inverse className="w-50" />
-        </a>
-        </Col>
-        <Col xs="1" className="mr-3">
-        <a href="#">
-          <FontAwesomeIcon icon={['fab', 'github']} className="w-50" />
-        </a>
-        </Col>
-      </Row>
-    </div>
-  </section>
-)
+  constructor(props) {
+    super(props);
+    library.add(fab);
+
+    this.state = {
+      date: this.diff_years(new Date('October 1, 2008 00:00:00'), new Date())
+    }
+  }
+
+  diff_years(dt2, dt1)
+  {
+    var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= (60 * 60 * 24);
+    return Math.abs(Math.round(diff/365.25));
+  }
+
+  render() {
+    return (
+      <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+        <div className="w-100">
+          <Row>
+            <Col>
+              <h1 className="mb-0">Anthony
+                <span className="text-primary"> Sherwin</span>
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="subheading mb-5">70A Shady Grove, Forest Hill, 3131
+                <a href="mailto:anthony.j.sherwin@gmail.com"> anthony.j.sherwin@gmail.com</a>
+              </h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className="lead mb-5">A results-driven Software Engineer with over {this.state.date} years' experience in assessing business needs, defining requirements and developing software solutions. Skilled in managing projects, developing new code, software design, building robust and scalable software, working effectively within a close knit team environment and achieving strict deadlines.</p>
+            </Col>
+          </Row>
+          <Row className="social-icons">
+            <Col xs="1" className="mr-3">
+            <a href="https://www.linkedin.com/in/anthony-sherwin-404a942b">
+              <FontAwesomeIcon icon={['fab', 'linkedin']} inverse className="w-50" />
+            </a>
+            </Col>
+            <Col xs="1" className="mr-3">
+            <a href="https://github.com/asherwin86/resume">
+              <FontAwesomeIcon icon={['fab', 'github']} className="w-50" />
+            </a>
+            </Col>
+          </Row>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default About
